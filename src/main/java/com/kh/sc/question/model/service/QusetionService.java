0f014c9babd907863a3e74bda91879f93847a754
남 <@DESKTOP-6PRL61I>
2020-01-09 15:44:13 +0900

    nss

--- src/main/java/com/kh/sc/question/model/service/QusetionService.java
+++ src/main/java/com/kh/sc/question/model/service/QusetionService.java
@@ -1,10 +1,14 @@
 package com.kh.sc.question.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.sc.question.model.dao.QuestionDAO;
 
 @Service
 public class QusetionService {
 
-	
+	@Autowired
+	QuestionDAO qdao;
 	
 }
